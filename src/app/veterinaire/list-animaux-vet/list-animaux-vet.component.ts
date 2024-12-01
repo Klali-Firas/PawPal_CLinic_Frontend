da2be517e978b7dc842f0b0f47618b9ab98d5d2e
Roua Fatnassi <roua.fatnassi.02@gmail.com>
2024-12-01 23:08:59 +0100

    ajouteAnimalProp&&ModifierAnimalVet

--- src/app/veterinaire/list-animaux-vet/list-animaux-vet.component.ts
+++ src/app/veterinaire/list-animaux-vet/list-animaux-vet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Animaux, Utilisateurs } from 'src/app/interfaces/interfaces';
 import { AnimauxService } from 'src/app/services/animaux.service';
 import { UtilisateurService } from 'src/app/services/utilisateur.service';
@@ -10,10 +11,22 @@ import { UtilisateurService } from 'src/app/services/utilisateur.service';
 })
 export class ListAnimauxVetComponent implements OnInit {
   animauxList: Animaux[] = [];
-
   proprietaires: Map<number, Utilisateurs> = new Map<number, Utilisateurs>();
+  showEditAnimalPopup = false;
+  editAnimalForm: FormGroup;
+  selectedAnimal: Animaux | null = null;
 
-  constructor(private animauxService: AnimauxService, private userService: UtilisateurService) { }
+  constructor(
+    private fb: FormBuilder,
+    private animauxService: AnimauxService,
+    private userService: UtilisateurService
+  ) {
+    this.editAnimalForm = this.fb.group({
+      nom: ['', Validators.required],
+      race: [''],
+      age: [null, [Validators.required, this.nonNegativeValidator]],
+    });
+  }
 
   ngOnInit(): void {
     this.animauxService.getAllAnimaux().subscribe({
@@ -23,7 +36,6 @@ export class ListAnimauxVetComponent implements OnInit {
           this.getProprietaireByAnimalId(animal.id);
         });
       },
-
       error: (error) => console.error('There was an error!', error)
     });
   }
@@ -35,5 +47,44 @@ export class ListAnimauxVetComponent implements OnInit {
     });
   }
 
+  openEditAnimalPopup(animal: Animaux): void {
+    this.selectedAnimal = animal;
+    this.editAnimalForm.patchValue({
+      nom: animal.nom,
+      race: animal.race,
+      age: animal.age,
+    });
+    this.showEditAnimalPopup = true;
+  }
 
+  closeEditAnimalPopup(): void {
+    this.showEditAnimalPopup = false;
+    this.selectedAnimal = null;
+  }
+
+  onEditAnimal(): void {
+    if (this.editAnimalForm.valid && this.selectedAnimal) {
+      const updatedAnimal: Animaux = {
+        ...this.selectedAnimal,
+        ...this.editAnimalForm.value,
+      };
+
+      this.animauxService.updateAnimaux(updatedAnimal.id, updatedAnimal).subscribe({
+        next: (response: Animaux) => {
+          const index = this.animauxList.findIndex(animal => animal.id === response.id);
+          if (index !== -1) {
+            this.animauxList[index] = response;
+          }
+          this.closeEditAnimalPopup();
+        },
+        error: (error: any) => {
+          console.error('Error updating animal', error);
+        }
+      });
+    }
+  }
+
+  nonNegativeValidator(control: AbstractControl): ValidationErrors | null {
+    return control.value !== null && control.value < 0 ? { nonNegative: true } : null;
+  }
 }
